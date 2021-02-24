5f2e849f82ee922393d017a67b3318dd1aacb45c
Eliza <Gilbert>
2021-02-23 22:36:36 -0700

    Combined remaining years

--- R_scripts/01_DataWranglingET_Toolbox.R
+++ R_scripts/01_DataWranglingET_Toolbox.R
@@ -12,6 +12,22 @@ library(stringi)
 #load data####
 dat <- read_csv("Data/Raw/ET_toolbox_R6_2019.csv", skip = 18)
 dat <- read_csv("Data/Raw/ET_toolbox_R6_2018.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2017.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2016.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2015.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2014.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2013.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2012.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2011.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2010.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2009.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2008.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2007.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2006.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2005.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2004.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2003.csv", skip = 18)
+dat <- read_csv("Data/Raw/ET_toolbox_R6_2002.csv", skip = 18)
 
 #format data into a readable data frame####
 #rename column
@@ -27,6 +43,11 @@ dat <- t(apply(dat,1,function(x){
 }))
 dat <- as.data.frame(dat)
 dat <- dat[-c(13)]
+
+             #2011, 2009, 2005, 2004, 2003, 2002 data
+            dat <- dat[-c(13:14)]
+             #2010, 2008, 2007, 2006 data
+            dat <- dat[-c(13)]
 
 #formatting for column names, dates, variable classes####
 dat <- dat %>% 
@@ -49,12 +70,30 @@ dat <- dat %>%
   select(Date:Month, Month.n, Day:Use_to_date_since_Jan1)
 
 #to combine and write files
-dat2019 <- dat
-dat2018 <- dat
+dat2019 <- dat #346 obs
+dat2018 <- dat #345 obs
+dat2017 <- dat #365 obs
+dat2016 <- dat #365 obs
+dat2015 <- dat #365 obs
+dat2014 <- dat #365 obs
+dat2013 <- dat #174 obs
+dat2012 <- dat #366 obs
+dat2011 <- dat #363 obs
+dat2010 <- dat #363 obs
+dat2009 <- dat #363 obs
+dat2008 <- dat #364 obs
+dat2007 <- dat #363 obs
+dat2006 <- dat #363 obs
+dat2005 <- dat #333 obs
+dat2004 <- dat #359 obs
+dat2003 <- dat #350 obs
+dat2002 <- dat #360 obs
 
-Et_data <- rbind(dat2019,dat2018)
+Et_data <- rbind(dat2019,dat2018, dat2017, dat2016, dat2015, dat2014,
+                 dat2013, dat2012, dat2011, dat2010, dat2009, dat2008,
+                 dat2007, dat2006, dat2005, dat2004, dat2003, dat2002)
 write.csv(Et_data,"Data/Processed/ET_data.csv", row.names = FALSE)
-
+ 
 
 
 
