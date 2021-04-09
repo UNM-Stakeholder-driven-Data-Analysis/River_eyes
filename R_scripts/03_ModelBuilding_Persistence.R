2d70569472b33f9e220488cf638f71fe13ea2950
Eliza <Gilbert>
2021-04-08 18:13:29 -0600

    Failing misserably at linear modeling
    river mile persistence

--- R_scripts/03_ModelBuilding_Persistence.R
+++ R_scripts/03_ModelBuilding_Persistence.R
@@ -1,6 +1,13 @@
 #Read me ####
 #The purpose of this script is to construct linear models to predict
-#the extent of drying
+#the extent of drying 
+#1) first with section of river RM 100 to 74 that often has drying using a lmer with year and RM as random with non_ET toolbox predictors
+#2) assess temporal autocorrelation 
+#3) assess correlation among predictors
+#4) construct glmer with predictors
+#5) assess residuals of models for heteroscedactisty/normality/autocorrelation
+#6) AIC model selection
+#7) look at top model summary
 
 #Libraries ####
 library(tidyverse)
@@ -10,68 +17,150 @@ library(car) # for Anova(), vif()
 library(MuMIn) # for AICc
 library(emmeans)
 library(DHARMa)
+library(psych)
+library(forecast)
+library(nlme)
+library(sjPlot)
+library(effects)
 
-#Load data and combine response and predictors ####
-    #summed the extent dry by reach for each day
-response <- read.csv("Data/Processed/AnnualDryRM.csv") %>% 
-  filter(Year < 2012)
+#Load and wrangle data sets ####
+resp_dat <- read.csv('Data/Processed/AnnualDryRM.csv')
+resp_dat74 <- resp_dat %>% 
+  filter(RM==74) %>% 
+  arrange(Year)
 
-predictors <- read.csv("Data/Processed/Predictors_mn_sum.csv") %>% 
+pred_gages <- read.csv("Data/Processed/Predictors.csv")
+pred_gages_sum <- pred_gages%>% 
+  select(Date, Isleta_mean_daily_div_cfs, SanAcacia_mean_daily_div_cfs, Mean_cfs_SanAcacia) %>% 
   mutate(Date = as.Date(Date)) %>% 
   mutate(Year = lubridate::year(Date)) %>% 
-  select(Year, Isleta_mean_daily_div_cfs:Mean_cfs_SanAcacia,Ag_sum:Rain_sum, everything() ) %>% 
-  filter(Year < 2012)
+  group_by(Year) %>% 
+  summarise(across(Isleta_mean_daily_div_cfs:Mean_cfs_SanAcacia, sum, na.rm=TRUE)) %>% 
+  arrange(Year)
+otowi <- read.csv("Data/Processed/OtowiIndex.csv") %>% 
+  select(!X) %>% 
+  arrange(Year)
 
-predictors2 <- predictors %>% 
-  group_by(Year)%>% 
-  summarise(across(Isleta_mean_daily_div_cfs:Rain_sum, mean, na.rm=TRUE)) %>% 
-  rename(Ag_yr_mn = Ag_sum, Rip_yr_mn = Rip_sum, OW_yr_mn = OW_sum, Rain_yr_mn = Rain_sum,
-         Isleta_div = Isleta_mean_daily_div_cfs, SanA_div = SanAcacia_mean_daily_div_cfs,
-         SanA_gage = Mean_cfs_SanAcacia)
+dat74 <- cbind(resp_dat74$Sum_days_rm_dry,pred_gages_sum, otowi$Index_kaf,resp_dat74$RM) %>% 
+  select(Year, everything()) %>% 
+  rename(DaysDry=names(.)[2], Isleta_div_sum=Isleta_mean_daily_div_cfs, 
+         SanAcacia_div_sum=SanAcacia_mean_daily_div_cfs, SanAcacia_gage_sum=Mean_cfs_SanAcacia,
+         OtowiIndex = names(.)[6], RiverMile=names(.)[7])
 
-dat <- response %>% 
-  full_join(predictors2, by=c("Year")) 
+#Assess predictor correlations and reduce ####
 
-#Initial plots
-par(mfrow=c(2,1))
-plot(dat$Sum_days_rm_dry~dat$Year, main = "")
-hist(dat$Sum_days_rm_dry, main = "")
+pairs.panels(dat74[,3:6], scale=T)
+pairs.panels(dat74[,3:6], scale=F) #SanAcaciaGage and Otowi 0.96 and removed, #SanAGage and diversion/Otow 0.58 and 0.55 can I keep...I kept for now
 
-par(mfrow=c(2,4))
-plot(dat$Isleta_div~dat$Sum_days_rm_dry, main="IsletaDiv")
-plot(dat$SanA_div~dat$Sum_days_rm_dry, main="SanAcaciaDiv")
-plot(dat$SanA_gage~dat$Sum_days_rm_dry, main="SanAcaciaGage")
-plot(dat$Ag_yr_mn~dat$Sum_days_rm_dry, main="Ag")
-plot(dat$Rip_yr_mn~dat$Sum_days_rm_dry, main="Riparian")
-plot(dat$OW_yr_mn~dat$Sum_days_rm_dry, main="OpenWater")
-plot(dat$Rain_yr_mn~dat$Sum_days_rm_dry, main="Rain")
+dat74rev <- dat74 %>% 
+  select(!SanAcacia_gage_sum) %>% 
+  mutate(Year.f = as.character(Year))
+
+#GLM build models mile 74 DOESN'T PASS ASSUMPTIONS ####
+ #building all possible models
+mod_null <- glm(DaysDry ~ 1 +(1|Year), data = dat74rev)
+
+mod_full <- glm(DaysDry ~ scale(Isleta_div_sum) + scale(SanAcacia_div_sum) + scale(OtowiIndex) +(1|Year), data = dat74rev) 
+qqnorm(resid(mod_full)) #Passes kind of but looks sinusoidal
+qqline(resid(mod_full))
+plot(resid(mod_full)) # Does not look random, rather like a sign wave
+Acf(resid(mod_full)) # Passes
+
+mod_ID_SD <- glm(DaysDry ~ scale(Isleta_div_sum) + scale(SanAcacia_div_sum) + 
+                     (1|Year), data = dat74rev) 
+qqnorm(resid(mod_ID_SD)) #Passes kind of but looks sinusoidal
+qqline(resid(mod_ID_SD))
+plot(resid(mod_ID_SD)) # Does not look random, rather like a sign wave
+Acf(resid(mod_ID_SD)) #Passes
+
+mod_ID_OT <- glm(DaysDry ~ scale(Isleta_div_sum) + scale(OtowiIndex) + 
+                     (1|Year), data = dat74rev)
+qqnorm(resid(mod_ID_OT)) #Passes kind of but looks sinusoidal
+qqline(resid(mod_ID_OT))
+plot(resid(mod_ID_OT)) # Does not look random, rather like a sign wave
+Acf(resid(mod_ID_OT)) #Passes
+
+mod_SD_OT <- glm(DaysDry ~ scale(SanAcacia_div_sum) + scale(OtowiIndex) + 
+                     (1|Year), data = dat74rev)
+qqnorm(resid(mod_SD_OT)) #Passes kind of but looks sinusoidal
+qqline(resid(mod_SD_OT))
+plot(resid(mod_SD_OT)) # Does not look random, rather like a sign wave
+Acf(resid(mod_SD_OT)) #Passes
+
+mod_ID <- glm(DaysDry ~ scale(Isleta_div_sum) + (1|Year), data = dat74rev)
+qqnorm(resid(mod_ID)) # Doesn't really passes
+qqline(resid(mod_ID))
+plot(resid(mod_ID)) # Does not look random, rather like a sign wave
+Acf(resid(mod_ID)) #Passes
+
+mod_SD <- glm(DaysDry ~ scale(SanAcacia_div_sum) + (1|Year),  data = dat74rev)
+qqnorm(resid(mod_SD)) # Not sure really passes
+qqline(resid(mod_SD))
+plot(resid(mod_SD)) # Does not look random, rather like a sign wave
+Acf(resid(mod_SD)) #Passes
+
+mod_OT <- glm(DaysDry ~ scale(OtowiIndex) + (1|Year),  data = dat74rev)
+qqnorm(resid(mod_OT)) # Not sure if it really passes
+qqline(resid(mod_OT))
+plot(resid(mod_OT)) # Does not look random, rather like a sign wave
+Acf(resid(mod_OT)) #Passes
+
+#DON'T REALLY THINK ANY OF THESE PASS
+
+#Load and wrangle data sets ####
+resp_dat <- read.csv('Data/Processed/AnnualDryRM.csv')
+resp_dat74_100 <- resp_dat %>% 
+  filter(RM>=74 & RM<=100) %>% 
+  arrange(Year)
 
 
+pred_gages <- read.csv("Data/Processed/Predictors.csv")
+pred_gages_sum <- pred_gages%>% 
+  select(Date, Isleta_mean_daily_div_cfs, SanAcacia_mean_daily_div_cfs, Mean_cfs_SanAcacia) %>% 
+  mutate(Date = as.Date(Date)) %>% 
+  mutate(Year = lubridate::year(Date)) %>% 
+  group_by(Year) %>% 
+  summarise(across(Isleta_mean_daily_div_cfs:Mean_cfs_SanAcacia, sum, na.rm=TRUE)) %>% 
+  arrange(Year)
+otowi <- read.csv("Data/Processed/OtowiIndex.csv") %>% 
+  select(!X) %>%
+  rename(Year2=Year)
+  arrange(Year2)
+predictors <- cbind(pred_gages_sum, otowi) %>% 
+  select(!Year2)
 
-#Build models and check assumptions
-mod1 <- lmer(log(Sum_days_rm_dry+1) ~ Isleta_div + SanA_div + SanA_gage + Ag_yr_mn + Rip_yr_mn + OW_yr_mn +
-              Rain_yr_mn + (1|Year) +(1|RM), data=dat, REML = FALSE)
+dat_full <- resp_dat %>% 
+  full_join(predictors, by =c("Year")) %>% 
+  rename(DaysDry=Sum_days_rm_dry, Isleta_div_sum=Isleta_mean_daily_div_cfs, 
+         SanAcacia_div_sum=SanAcacia_mean_daily_div_cfs, SanAcacia_gage_sum=Mean_cfs_SanAcacia,
+         OtowiIndex = Index_kaf, RiverMile=RM)
 
- #check assumptions
-par(mfrow=c(1,1))
-simulateResiduals(mod1, plot = T)
+dat_full2 <- resp_dat74_100 %>% 
+  full_join(predictors, by =c("Year")) %>% 
+  rename(DaysDry=Sum_days_rm_dry, Isleta_div_sum=Isleta_mean_daily_div_cfs, 
+         SanAcacia_div_sum=SanAcacia_mean_daily_div_cfs, SanAcacia_gage_sum=Mean_cfs_SanAcacia,
+         OtowiIndex = Index_kaf, RiverMile=RM)
 
-plot(mod1)
-qqnorm(resid(mod1))
-qqline(resid(mod1))
+#GLMER with river miles 74 to 100 and full data set DOESN'T PASS ASSUMPTIONS #####
+mod_null <- glmer(DaysDry ~ 1 + (1|Year) + (1|RiverMile), family = "poisson", data = dat_full2)
+simulateResiduals(mod_null, plot = T)
+Acf(resid(mod_null))
 
-summary(mod1)
-car::vif(mod1)
+mod_full <- glmer((DaysDry)^2 ~ scale(Isleta_div_sum) + scale(SanAcacia_div_sum) + scale(OtowiIndex) + 
+                    (1|Year), family = "poisson", data = dat_full2) 
+simulateResiduals(mod_full, plot = T) #Does not pass with raw, log, or square root and too few data points
+Acf(resid(mod_full)) #Does not pass
 
-#remove variable because of vif
-mod2 <- lmer(log(Sum_days_rm_dry+1) ~ Isleta_div + SanA_div + SanA_gage + Ag_yr_mn + (1|Year) +(1|RM), data=dat, REML = FALSE)
+mod_null <- glmer(DaysDry ~ 1 + (1|Year) + (1|RiverMile), family = "poisson", data = dat_full2)
+simulateResiduals(mod_null, plot = T)
+Acf(resid(mod_null))
 
-par(mfrow=c(1,1))
-simulateResiduals(mod1, plot = T)
+mod_full <- glmer((DaysDry)^2 ~ scale(Isleta_div_sum) + scale(SanAcacia_div_sum) + scale(OtowiIndex) + 
+                    (1|Year), family = "poisson", data = dat_full2) 
+simulateResiduals(mod_full, plot = T) #Does not pass with raw, log, or square root and too few data points
+Acf(resid(mod_full)) #Does not pass
 
-plot(mod2)
-qqnorm(resid(mod2))
-qqline(resid(mod2))
-
-summary(mod2)
-car::vif(mod2)
+#LMER DOESN'T work ##### 
+#mod_ID_SD <- lmer(DaysDry ~ scale(Isleta_div_sum) + scale(SanAcacia_div_sum) + 
+                   # (1|Year.f), data = dat74rev) 
+#"ERROR: NUMBER OF LEVELS OF EACH GROUPING FACTOR MUST BE <NUMBER OF OBSERVATIONS (PROBLEMS:YEAR)
